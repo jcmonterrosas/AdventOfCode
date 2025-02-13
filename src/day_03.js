19851e86c39e7aed5671b61cc6b9f0ad6d105e91
jcmonterrosas <jcmonterrosas@unal.edu.co>
2025-02-13 20:40:28 +0100

    Implement solutions for Day 3 puzzles

--- src/day_03.js
+++ src/day_03.js
@@ -12,6 +12,30 @@
  */
 export const levelOne = ({ input, lines }) => {
   // your code here
+  let grid = new Map();
+  let x = 0, y = 0;
+  grid.set(`${x},${y}`, 1);
+
+  for (const dir of input) {
+    switch (dir) {
+      case '^':
+        y++;
+        break;
+      case 'v':
+        y--;
+        break;
+      case '>':
+        x++;
+        break;
+      case '<':
+        x--;
+        break;
+    }
+    const key = `${x},${y}`;
+    grid.set(key, (grid.get(key) || 0) + 1);    
+  }
+
+  return grid.size;
 };
 
 /**
@@ -23,4 +47,47 @@ export const levelOne = ({ input, lines }) => {
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
+  let grid = new Map();
+  let santaX = 0, santaY = 0, robotX = 0, robotY = 0;
+  grid.set(`${santaX},${santaY}`, 2);
+
+  for (let i = 0; i < input.length; i++) {      
+    if (i % 2 === 0) {
+      switch (input[i]) {
+        case '^':
+          santaY++;
+          break;
+        case 'v':
+          santaY--;
+          break;
+        case '>':
+          santaX++;
+          break;
+        case '<':
+          santaX--;
+          break;
+      }
+      const key = `${santaX},${santaY}`; 
+      grid.set(key, (grid.get(key) || 0) + 1); 
+    }else {
+      switch (input[i]) {
+        case '^':
+          robotY++;
+          break;
+        case 'v':
+          robotY--;
+          break;
+        case '>':
+          robotX++;
+          break;
+        case '<':
+          robotX--;
+          break;
+      }
+      let key = `${robotX},${robotY}`; 
+      grid.set(key, (grid.get(key) || 0) + 1); 
+    }
+  }
+  
+  return grid.size;
 };
