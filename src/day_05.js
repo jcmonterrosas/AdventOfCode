506b70539fd5ba5b2b720fc541daf860a06d42b8
jcmonterrosas <jcmonterrosas@unal.edu.co>
2025-02-16 00:09:47 +0100

    Add solutions for Day 5 puzzles and update completion progress

--- src/day_05.js
+++ src/day_05.js
@@ -12,6 +12,43 @@
  */
 export const levelOne = ({ input, lines }) => {
   // your code here
+  // Nice string => 3+ vowels, at least one letter that appears twice in a row, and none of the strings ab, cd, pq, or xy
+  // lines = ["ugknbfddgicrmopn", "aaa", "jchzalrnumimnmhp", "haegwjzuvuyypxyu", "dvszwmarrgswjxmb"];
+  let niceStrings = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  let disallowedStrings = ["ab", "cd", "pq", "xy"];
+
+  for (const string of lines) {
+    let vowelsCount = 0;
+    let hasDoubleLetter = false;
+    let hasDisallowedString = false;
+
+    for (const char of string) {
+      if (vowels.includes(char)) {
+        vowelsCount++;
+      }
+    }
+
+    for (let i = 0; i < string.length - 1; i++) {
+      if (string[i] === string[i + 1]) {
+        hasDoubleLetter = true;
+        break;
+      }
+    }
+
+    for (const disallowed of disallowedStrings) {
+      if (string.includes(disallowed)) {
+        hasDisallowedString = true;
+        break;
+      }
+    }
+
+    if (vowelsCount >= 3 && hasDoubleLetter && !hasDisallowedString) {
+      niceStrings++;
+    }
+  }
+
+  return niceStrings;
 };
 
 /**
@@ -23,4 +60,27 @@ export const levelOne = ({ input, lines }) => {
  */
 export const levelTwo = ({ input, lines }) => {
   // your code here
+  // lines = ["qjhvhtzxzqqjkmpb", "xxyxx", "uurcxstgmygtbstg", "ieodomkazucvgmuy"];
+  let niceStrings = 0;
+
+  for (const string of lines) {
+    let hasDoubleLetterPair = false;
+    let hasRepeatLetter = false;
+
+    for (let i = 0; i < string.length - 2; i++) {
+      let pair = string[i] + string[i + 1];
+
+      if (string.substring(i + 2).includes(pair)) {
+        hasDoubleLetterPair = true;
+      }
+
+      if (string[i] === string[i + 2]) {
+        hasRepeatLetter = true;
+      }
+    }
+    if (hasDoubleLetterPair && hasRepeatLetter) {
+      niceStrings++;
+    }
+  }
+  return niceStrings;
 };
